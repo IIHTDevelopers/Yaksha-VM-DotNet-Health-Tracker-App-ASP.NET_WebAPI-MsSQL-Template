6e1095352fed54cad4bd12d9718258ec93327c09
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-06 08:08:27 +0530

    Add files via upload

---
+++ HealthTrackerApp/DAL/DbContext/HealthTrackerDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using HealthTrackerApp.Models;
+using System.Data.Entity;
+
+
+namespace HealthTrackerApp.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DefaultConnection") { }
+
+        public DbSet<Health> Healths { get; set; }
+    }
+}
---
+++ HealthTrackerApp/DAL/Services/Repository/IHealthTrackerRepository.cs
@@ -0,0 +1,19 @@
+﻿using HealthTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrackerApp.DAL.Services.Repository
+{
+    public interface IHealthTrackerRepository
+    {
+        List<Health> GetAllHealths();
+        Task<Health> CreateHealth(Health expense);
+        Task<Health> GetHealthById(long id);
+        Task<bool> DeleteHealthById(long id);
+        Task<Health> UpdateHealth(Health model);
+    }
+}
+
