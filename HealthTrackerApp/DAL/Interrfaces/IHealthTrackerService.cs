45ee08cbf8f71007f72a0a6d3c78bbbeedd944eb
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-07 18:05:30 +0530

    Add files via upload

---
+++ HealthTrackerApp/DAL/Interrfaces/IHealthTrackerService.cs
@@ -0,0 +1,18 @@
+﻿using HealthTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrackerApp.DAL.Interrfaces
+{
+    public interface IHealthTrackerService
+    {
+        List<Health> GetAllHealths();
+        Task<Health> CreateHealth(Health expense);
+        Task<Health> GetHealthById(long id);
+        Task<bool> DeleteHealthById(long id);
+        Task<Health> UpdateHealth(Health model);
+    }
+}
---
+++ HealthTrackerApp/DAL/Services/HealthTrackerService.cs
@@ -0,0 +1,51 @@
+﻿using HealthTrackerApp.DAL.Interrfaces;
+using HealthTrackerApp.DAL.Services.Repository;
+using HealthTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HealthTrackerApp.DAL.Services
+{
+    public class HealthTrackerService : IHealthTrackerService
+    {
+        private readonly IHealthTrackerRepository _repository;
+
+        public HealthTrackerService(IHealthTrackerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Health> CreateHealth(Health expense)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteHealthById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public List<Health> GetAllHealths()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Task<Health> GetHealthById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Task<Health> UpdateHealth(Health model)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
